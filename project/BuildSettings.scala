a13e7a24e568a0d91daadca8c0f188b26ecf95df
Ian Streeter <ian@snowplowanalytics.com>
2021-07-01 18:44:54 +0100

    Bump scala versions to 2.12.14 and 2.13.6 (close #78)

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -26,8 +26,8 @@ object BuildSettings {
 
   lazy val commonSettings = Seq(
     organization       := "com.snowplowanalytics",
-    scalaVersion       := "2.12.12",
-    crossScalaVersions := Seq("2.12.12", "2.13.3"),
+    scalaVersion       := "2.12.14",
+    crossScalaVersions := Seq("2.12.14", "2.13.6"),
     licenses += ("Apache-2.0", url("https://www.apache.org/licenses/LICENSE-2.0"))
   )
 
