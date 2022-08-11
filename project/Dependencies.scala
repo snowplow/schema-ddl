8842a64e998414dafe9c4bbeb06ae42dec4b4a00
Dilyan Damyanov <dilyand@gmail.com>
2022-08-11 12:13:38 +0100

    Bump cats-parse to 0.3.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -21,7 +21,7 @@ object Dependencies {
     val circe            = "0.14.2"
     val circeJackson     = "0.14.0"
     val jsonValidator    = "1.0.72"
-    val catsParse        = "0.3.4"
+    val catsParse        = "0.3.8"
     val libCompat        = "2.5.0"
     val jacksonDatabind  = "2.13.2.1"
 
