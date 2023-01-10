ecc52118690cf1306682663ae5a42545a5427a90
Ian Streeter <ian@snowplowanalytics.com>
2023-01-10 15:26:51 +0000

    Bump json-schema-validator to 1.0.76 (close #143)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
     val igluCore         = "1.1.2"
     val circe            = "0.14.3"
     val circeJackson     = "0.14.0"
-    val jsonValidator    = "1.0.72"
+    val jsonValidator    = "1.0.76"
     val catsParse        = "0.3.8"
     val libCompat        = "2.8.1"
     val jacksonDatabind  = "2.13.2.1"
