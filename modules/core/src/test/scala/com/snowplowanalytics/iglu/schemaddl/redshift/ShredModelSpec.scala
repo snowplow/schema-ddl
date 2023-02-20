b1173cad7ae8cd211207dbd38115640a6cb0afc0
pavel.voropaev <voropaev.p@gmail.com>
2023-02-20 19:30:51 +0000

    bugfix for not null field removal

--- modules/core/src/test/scala/com/snowplowanalytics/iglu/schemaddl/redshift/ShredModelSpec.scala
+++ modules/core/src/test/scala/com/snowplowanalytics/iglu/schemaddl/redshift/ShredModelSpec.scala
@@ -251,6 +251,51 @@ class ShredModelSpec extends Specification {
       )
     }
 
+    "should make a recovery model when not null field is removed" in {
+      val s1 = ShredModel.good(dummyKey,
+        json"""{
+               "type": "object",
+               "properties": {
+                 "foo": {
+                   "type": "string",
+                   "maxLength": 20
+                 }},
+              "required": ["foo"]
+              }""".schema)
+      val s2 = ShredModel.good(dummyKey1,
+        json"""{
+               "type": "object",
+               "properties": {
+                 "foo1": {
+                   "type": "number"
+                 }}
+              }""".schema)
+
+      s1.merge(s2).toTestString must beLeft(
+        """CREATE TABLE IF NOT EXISTS s.com_acme_example_1_1_0_recovered_1202144068 (
+          |  "schema_vendor"  VARCHAR(128)     ENCODE ZSTD NOT NULL,
+          |  "schema_name"    VARCHAR(128)     ENCODE ZSTD NOT NULL,
+          |  "schema_format"  VARCHAR(128)     ENCODE ZSTD NOT NULL,
+          |  "schema_version" VARCHAR(128)     ENCODE ZSTD NOT NULL,
+          |  "root_id"        CHAR(36)         ENCODE RAW  NOT NULL,
+          |  "root_tstamp"    TIMESTAMP        ENCODE ZSTD NOT NULL,
+          |  "ref_root"       VARCHAR(255)     ENCODE ZSTD NOT NULL,
+          |  "ref_tree"       VARCHAR(1500)    ENCODE ZSTD NOT NULL,
+          |  "ref_parent"     VARCHAR(255)     ENCODE ZSTD NOT NULL,
+          |  "foo1"           DOUBLE PRECISION ENCODE RAW,
+          |  FOREIGN KEY (root_id) REFERENCES s.events(event_id)
+          |)
+          |DISTSTYLE KEY
+          |DISTKEY (root_id)
+          |SORTKEY (root_tstamp);
+          |
+          |COMMENT ON TABLE s.com_acme_example_1_1_0_recovered_1202144068 IS 'iglu:com.acme/example/jsonschema/1-0-1';
+          |
+          |Making required column nullable foo""".stripMargin
+      )
+    }
+
+    
     "should make a ignore varchar narrowing" in {
       val s1 = ShredModel.good(dummyKey,
         json"""{
